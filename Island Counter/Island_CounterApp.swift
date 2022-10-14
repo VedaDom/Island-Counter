467c7fcef23738f31956e27313f3f507d1a2f5ea
Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>
2022-10-15 00:03:10 +0200

    Initial Commit

---
+++ Island Counter/Island_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Island_CounterApp.swift
+//  Island Counter
+//
+//  Created by Nsengimana Veda Dominique on 15/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Island_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
